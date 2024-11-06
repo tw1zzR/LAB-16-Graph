792844ac1b2505070d0677976ee4143e806aa532
tw1zzR <regenuss@gmail.com>
2024-11-06 12:59:24 +0200

    Completed LAB16.1

---
+++ lab16.1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as mp; import array as arr; import math
+
+x=[i for i in range(-3,6)]
+y=[]
+
+for elem in x:
+    if elem != 0:
+        value = 2 * math.sin(elem) + math.log2(abs(elem))
+    else:
+        value = 2 * math.sin(elem)
+    y.append(value)
+
+mp.plot(x,y, 'm--')
+mp.title("15.1")
+mp.xlabel("X-axis")
+mp.ylabel("Y-asis:")
+mp.savefig('page.svg')
+mp.show()
+
+print("Result has been written to 'page.svg'")
